8745127ab1b333953a108512dda10147daaa66cf
Stefan G <stefg@stefans-air.mynetworksettings.com>
2023-02-18 00:00:02 -0500

    Added the modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import Post from './components/Post';
+import { useState } from 'react';
+
+import PostsList from './components/PostsList';
+import MainHeader from './components/MainHeader';
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
   return (
-    <main>
-      <Post author='Stef' body='This is awesome' />
-      <Post author='Jackie' body='This something cool' />
-    </main>
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostsList isPosting={modalIsVisible} onStopPosting={hideModalHandler}/>
+      </main>
+    </>
   );
 }
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,16 +1,14 @@
 import classes from './Post.module.css';
 
 function Post(props) {
+  props.author;
+  props.body;
   return (
-    <div className={classes.post}>
+    <li className={classes.post}>
       <p className={classes.author}>{props.author}</p>
-      <p className={classes.body}>{props.body}</p>
-    </div>
+      <p className={classes.text}>{props.body}</p>
+    </li>
   );
 }
 
 export default Post;
-
-/*
-
-*/
